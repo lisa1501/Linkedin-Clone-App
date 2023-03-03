16ea6c6ae3f01dc24fc6ec804f708c93842c4054
halisa <halisasaipulaa@gmail.com>
2023-03-03 02:20:24 -0800

    implemented sign in functionality

--- pages/home.js
+++ pages/home.js
@@ -5,7 +5,7 @@ import GroupIcon from "@mui/icons-material/Group";
 import OndemandVideoSharpIcon from "@mui/icons-material/OndemandVideoSharp";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
-
+import { getProviders, signIn } from "next-auth/react";
 
 import Head from "next/head";
 
@@ -13,7 +13,7 @@ import HeaderLink from '../components/HeaderLink';
 
 
 
-const home = () => {
+const home = ({ providers }) => {
     
     return (
         <div className='space-y-10 relative'>
@@ -35,11 +35,18 @@ const home = () => {
                         <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
                     </div>
                     
-                    <div className='pl-4'>
-                        <button  className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2">
-                            Sign in
-                        </button>
-                    </div>
+                    {Object.values(providers).map((provider) => (
+                        <div key={provider.name}>
+                            <div className="pl-4">
+                                <button
+                                    className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2"
+                                    onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+                                >
+                                    Sign in
+                                </button>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </header>
 
@@ -72,6 +79,16 @@ const home = () => {
 }
 
 export default home;
+
+export async function getServerSideProps(context) {
+    const providers = await getProviders();
+  
+    return {
+      props: {
+        providers,
+      },
+    };
+  }
 
 
 
