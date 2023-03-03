57bb13b5ce9398a5cd51f9abef2855daf3aa9f27
halisa <halisasaipulaa@gmail.com>
2023-03-03 11:28:43 -0800

    rendered headerlinks on header component and added active, hidden two props to Headerlinkcomponent

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,16 @@
-import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import Image from "next/image";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import GroupIcon from "@mui/icons-material/Group";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { Avatar } from "@mui/material";
+import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
+
+
+
+import HeaderLink from "./HeaderLink";
 
 
 
@@ -17,6 +28,16 @@ const Header = () => {
                             placeholder-black/70 dark:placeholder-white/75 flex-grow"
                     />
                 </div>
+            </div>
+
+            <div className="flex items-center space-x-6">
+                <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
+                <HeaderLink Icon={GroupIcon} text="My Network" feed />
+                <HeaderLink Icon={BusinessCenterIcon} text="Jobs" feed hidden />
+                <HeaderLink Icon={ChatIcon} text="Messaging" feed />
+                <HeaderLink Icon={NotificationsIcon} text="Notifications" feed active/>
+                <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
+                <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
             </div>
 
         </header>
--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,9 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
-            {/* <button onClick={signOut}>
+            <button onClick={signOut}>
                 Sign Out
-            </button> */}
+            </button>
         </div>
     )
 }
