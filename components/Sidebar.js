bf786249e70fec2709c1d45f7084a66459441fee
halisa <halisasaipulaa@gmail.com>
2023-03-04 11:19:53 -0800

    added bottom section to sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Avatar } from "@mui/material";
 import { signOut } from 'next-auth/react';
 import BookmarkOutlinedIcon from "@mui/icons-material/BookmarkOutlined";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
 
 const Sidebar = () => {
     return (
@@ -54,12 +55,21 @@ const Sidebar = () => {
                         <BookmarkOutlinedIcon className="!-ml-1" />
                         <h4 className="dark:text-white font-medium">My items</h4>
                     </div>
-
-
                 </div>
             </div>
     
             {/* bottom */}
+            <div className="hidden md:flex bg-white dark:bg-[#1D2226] text-black/70 dark:text-white/75 rounded-lg overflow-hidden flex-col space-y-2 pt-2.5 sticky top-20 border border-gray-300 dark:border-none">
+                <p className="sidebarLink">Groups</p>
+                <div className="flex items-center justify-between">
+                    <p className="sidebarLink">Events</p>
+                    <AddRoundedIcon className="!h-5" />
+                </div>
+                <p className="sidebarLink">Followed Hashtags</p>
+                <div className="sidebarButton text-center">
+                    <h4 className="dark:text-white font-medium text-sm">Discover More</h4>
+                </div>
+            </div>
         </div>
     )
 }
