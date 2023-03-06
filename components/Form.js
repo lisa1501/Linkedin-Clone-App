59aa35d66ad02bf811cb9fcfa105810f50894f6c
halisa <halisasaipulaa@gmail.com>
2023-03-06 15:30:35 -0800

    added textarea to form component

--- components/Form.js
+++ components/Form.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import { useState } from "react";
 
 function Form() {
+    const [input, setInput] = useState("");
+    console.log(input)
     return (
-        <div>
-        Form
-        </div>
+        <form>
+            <textarea
+                rows="4"
+                placeholder="What do you want to talk about?"
+                className="bg-transparent focus:outline-none dark:placeholder-white/75"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+        </form>
     )
 }
 
