c03291f885979634e0b6d62700bec478dc454c1c
halisa <halisasaipulaa@gmail.com>
2023-03-08 12:36:36 -0800

    rendered user avatar and email on post content

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,6 +1,8 @@
+import { useSession } from 'next-auth/react'
 import React from 'react'
 
 const HeaderLink = ({ Icon, text, avatar, feed, active, hidden }) => {
+        const { data: session } = useSession();
     return (
         <div className={`${
             hidden && "hidden md:inline-flex"} cursor-pointer flex flex-col justify-center items-center ${
@@ -8,7 +10,7 @@ const HeaderLink = ({ Icon, text, avatar, feed, active, hidden }) => {
                 : "text-gray-500 hover:text-gray-700"} ${
             active && "!text-black dark:!text-white"}`}
         >
-            {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" />
+            {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" src={session?.user?.image} />
                     : <Icon /> }
             
             <h4 className={`text-sm $ ${feed && "hidden lg:flex justify-center w-full mx-auto"}`}>{text}</h4>
--- components/Post.js
+++ components/Post.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import { Avatar } from '@mui/material';
 
-function Post({ post }) {
+
+function Post({ post, modalPost }) {
     return (
-        <div>
-            <img src={post.photoUrl} alt=""/>
-            <p>{post.input}</p>
+        <div className={`bg-white dark:bg-[#1D2226] ${
+            modalPost ? "rounded-r-lg" : "rounded-lg"
+            } space-y-2 py-2.5 border border-gray-300 dark:border-none`}>
+            <div className="flex items-center px-2.5 cursor-pointer">
+                <Avatar src={post.userImg} className="!h-10 !w-10 cursor-pointer" />
+                <div className="mr-auto ml-2 leading-none">
+                    <h6 className="font-medium hover:text-blue-500 hover:underline">
+                        {post.username}
+                    </h6>
+                    <p className="text-sm dark:text-white/75 opacity-80">{post.email}</p>
+                    
+                </div>
+            </div>
         </div>
     )
 }
