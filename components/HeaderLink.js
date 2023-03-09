7fa29768efcf1a39fbc523bfc303a6c279b5aa6c
halisa <halisasaipulaa@gmail.com>
2023-03-09 11:39:54 -0800

    implemented headerlink component Me icon click for user signOut

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,4 +1,4 @@
-import { useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import React from 'react'
 
 const HeaderLink = ({ Icon, text, avatar, feed, active, hidden }) => {
@@ -9,6 +9,7 @@ const HeaderLink = ({ Icon, text, avatar, feed, active, hidden }) => {
             feed ? "text-black/60 hover:text-black dark:text-white/75 dark:hover:text-white lg:-mb-1.5 space-y-1"
                 : "text-gray-500 hover:text-gray-700"} ${
             active && "!text-black dark:!text-white"}`}
+            onClick={() => avatar && signOut()}
         >
             {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" src={session?.user?.image} />
                     : <Icon /> }
